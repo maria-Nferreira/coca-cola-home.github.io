659c69b2074995645371afaa69a2b958bef236af
Maria <mari4ferrei4@outlook.com>
2023-07-03 23:06:54 -0300

    upload coca-cola home

---
+++ script.js
@@ -0,0 +1,209 @@
+// COCA-COLA TRADICIONAL
+function red(){
+    // mudando a cor do background
+    document.body.style.background = 'var(--cor-fundo)'
+    document.body.style.transition = '1s all'
+
+    // mundando a imagem da lata
+    document.getElementById('imagem-lata').src ='imagens/cocacolatradicionalS.png'
+
+    // mudando a cor background dos botões latas
+    document.getElementById('lata-zero').style.background = 'var(--cor-fundo)'
+    document.getElementById('lata-zero').style.transition = '1s all'
+    document.getElementById('lata-light').style.background = 'var(--cor-fundo)'
+    document.getElementById('lata-light').style.transition = '1s all'
+    document.getElementById('lata-tradicional').style.background = 'var(--cor-fundo)'
+    document.getElementById('lata-tradicional').style.transition = '1s all'
+    
+   // mudando a cor do título principal e o texto
+    document.getElementById('titulo').style.color = 'white'
+    document.getElementById('titulo').style.transition = '1s all'
+    document.getElementById('texto').style.color = 'white'
+    document.getElementById('texto').style.transition = '1s all'
+
+    // mudando a imagem da logo
+    document.getElementById('logo').src = 'imagens/logo-nav.png'
+    
+    // mudando a cor o background e cor do botão de produtos
+    document.getElementById('botao-produto').style.background = 'var(--cor-texto)'
+    document.getElementById('botao-produto').style.color = 'var(--cor-lata-zero)'
+    document.getElementById('botao-produto').style.transition = '1s all'
+
+    // mudando a cor dos links do menu
+    document.getElementById('link-nav-1').style.color = 'white'
+    document.getElementById('link-nav-1').style.transition = '1s all'
+    document.getElementById('link-nav-2').style.color = 'white'
+    document.getElementById('link-nav-2').style.transition = '1s all'
+    document.getElementById('link-nav-3').style.color = 'white'
+    document.getElementById('link-nav-3').style.transition = '1s all'
+    document.getElementById('link-nav-4').style.color = 'white'
+    document.getElementById('link-nav-4').style.transition = '1s all'
+
+    // mudando a cor e background do botao de ENTRAR
+    document.getElementById('botao-entrar').style.color = 'white'
+    document.getElementById('botao-entrar').style.background = 'var(--cor-fundo)'
+    document.getElementById('botao-entrar').style.borderColor = 'white'
+    document.getElementById('botao-entrar').style.transition = '1s all'
+
+    // mudando a cor dos links REDES SOCIAIS
+    document.getElementById('facebook').style.color = 'white'
+    document.getElementById('facebook').style.transition = '1s all'
+    document.getElementById('instagram').style.color = 'white'
+    document.getElementById('instagram').style.transition = '1s all'
+    document.getElementById('twitter').style.color = 'white'
+    document.getElementById('twitter').style.transition = '1s all'
+    document.getElementById('youtube').style.color = 'white'
+    document.getElementById('youtube').style.transition = '1s all'
+
+    // mudando a cor do botão mobile
+    document.getElementById('botao-mobile').style.color = 'white'
+    document.getElementById('botao-mobile').style.borderColor = 'white'
+    document.getElementById('botao-mobile').style.background = 'var(--cor-fundo)'
+    document.getElementById('botao-mobile').style.transition = '1s all'
+}
+
+
+
+
+
+// COCA-COLA LIGHT
+function gray(){
+    // mudando a cor do background
+    document.body.style.background = 'var(--cor-lata-light)'
+    document.body.style.transition = '1s all'
+
+    // mundando a imagem da lata
+    document.getElementById('imagem-lata').src ='imagens/cocacolalight.png'
+
+    // mudando a cor background dos botões latas
+    document.getElementById('lata-zero').style.background = 'var(--cor-lata-light)'
+    document.getElementById('lata-zero').style.transition = '1s all'
+    document.getElementById('lata-light').style.background = 'var(--cor-lata-light)'
+    document.getElementById('lata-light').style.transition = '1s all'
+    document.getElementById('lata-tradicional').style.background = 'var(--cor-lata-light)'
+    document.getElementById('lata-tradicional').style.transition = '1s all'
+    
+    // mudando a cor do título principal e o texto
+    document.getElementById('texto').style.color = 'var(--cor-lata-zero)'
+    document.getElementById('texto').style.transition = '1s all'
+    document.getElementById('titulo').style.color = 'var(--cor-lata-zero)'
+    document.getElementById('titulo').style.transition = '1s all'
+    
+    // mudando a imagem da logo
+    document.getElementById('logo').src = 'imagens/Coca-Cola_logo.png'
+    
+    // mudando a cor o background e cor do botão de produtos
+    document.getElementById('botao-produto').style.background = 'var(--cor-lata-zero)'
+    document.getElementById('botao-produto').style.color = 'var(--cor-texto)'
+    document.getElementById('botao-produto').style.transition = '1s all'
+
+    // mudando a cor dos links do menu
+    document.getElementById('link-nav-1').style.color = 'var(--cor-lata-zero)'
+    document.getElementById('link-nav-1').style.transition = '1s all'
+    document.getElementById('link-nav-2').style.color = 'var(--cor-lata-zero)'
+    document.getElementById('link-nav-2').style.transition = '1s all'
+    document.getElementById('link-nav-3').style.color = 'var(--cor-lata-zero)'
+    document.getElementById('link-nav-3').style.transition = '1s all'
+    document.getElementById('link-nav-4').style.color = 'var(--cor-lata-zero)'
+    document.getElementById('link-nav-4').style.transition = '1s all'
+
+
+    // mudando a cor e background do botao de ENTRAR
+    document.getElementById('botao-entrar').style.color = 'var(--cor-lata-zero)'
+    document.getElementById('botao-entrar').style.background = 'var(--cor-lata-light)'
+    document.getElementById('botao-entrar').style.borderColor = 'var(--cor-lata-zero)'
+    document.getElementById('botao-entrar').style.transition = '1s all'
+    
+
+    
+
+    // mudando a cor dos links REDES SOCIAIS
+    document.getElementById('facebook').style.color = 'var(--cor-lata-zero)'
+    document.getElementById('facebook').style.transition  = '1s all'
+    document.getElementById('instagram').style.color = 'var(--cor-lata-zero)'
+    document.getElementById('instagram').style.transition = '1s all'
+    document.getElementById('twitter').style.color = 'var(--cor-lata-zero)'
+    document.getElementById('twitter').style.transition = '1s all'
+    document.getElementById('youtube').style.color = 'var(--cor-lata-zero)'
+    document.getElementById('youtube').style.transition = '1s all'
+
+
+    // mudando a cor do botão mobile
+    document.getElementById("botao-mobile").style.color = "var(--cor-lata-zero)"
+    document.getElementById("botao-mobile").style.borderColor = "var(--cor-lata-zero)"
+    document.getElementById("botao-mobile").style.background = "var(--cor-lata-light)"
+    document.getElementById('botao-mobile').style.transition = '1s all'
+    
+}
+
+
+
+
+
+
+
+// COCA-COLA ZERO
+function dark(){
+    // mudando a cor do background
+    document.body.style.background = 'var(--cor-lata-zero)'
+    document.body.style.transition = '1s all'
+
+    // mundando a imagem da lata
+    document.getElementById('imagem-lata').src ='imagens/cocacolazero.png'
+
+    // mudando a cor background dos botões latas
+    document.getElementById('lata-zero').style.background = 'var(--cor-lata-zero)'
+    document.getElementById('lata-zero').style.transition = '1s all'
+    document.getElementById('lata-light').style.background = 'var(--cor-lata-zero)'
+    document.getElementById('lata-light').style.transition = '1s all'
+    document.getElementById('lata-tradicional').style.background = 'var(--cor-lata-zero)'
+    document.getElementById('lata-tradicional').style.transition = '1s all'
+
+    // mudando a cor do título principal e o texto
+    document.getElementById('titulo').style.color = 'white'
+    document.getElementById('titulo').style.transition = '1s all'
+    document.getElementById('texto').style.color = 'white'
+    document.getElementById('texto').style.transition = '1s all'
+
+    // mudando a imagem da logo
+    document.getElementById('logo').src = 'imagens/logo-nav.png'
+    
+    // mudando a cor o background e cor do botão de produtos
+    document.getElementById('botao-produto').style.background = 'var(--cor-texto)'
+    document.getElementById('botao-produto').style.color = 'var(--cor-lata-zero)'
+    document.getElementById('botao-produto').style.transition = '1s all'
+
+    // mudando a cor dos links do menu
+    document.getElementById('link-nav-1').style.color = 'white'
+    document.getElementById('link-nav-1').style.transition = '1s all'
+    document.getElementById('link-nav-2').style.color = 'white'
+    document.getElementById('link-nav-2').style.transition = '1s all'
+    document.getElementById('link-nav-3').style.color = 'white'
+    document.getElementById('link-nav-3').style.transition = '1s all'
+    document.getElementById('link-nav-4').style.color = 'white'
+    document.getElementById('link-nav-4').style.transition = '1s all'
+
+    
+    // mudando a cor e background do botao de ENTRAR
+    document.getElementById('botao-entrar').style.color = 'white'
+    document.getElementById('botao-entrar').style.background = 'var(--cor-lata-zero)'
+    document.getElementById('botao-entrar').style.borderColor = 'white'
+    document.getElementById('botao-entrar').style.transition = '1s all'
+
+    // mudando a cor dos links REDES SOCIAIS
+    document.getElementById('facebook').style.color = 'white'
+    document.getElementById('facebook').style.transition = '1s all'
+    document.getElementById('instagram').style.color = 'white'
+    document.getElementById('instagram').style.transition = '1s all'
+    document.getElementById('twitter').style.color = 'white'
+    document.getElementById('twitter').style.transition = '1s all'
+    document.getElementById('youtube').style.color = 'white'
+    document.getElementById('youtube').style.transition = '1s all'
+
+    // mudando a cor do botão mobile
+    document.getElementById('botao-mobile').style.color = 'white'
+    document.getElementById('botao-mobile').style.borderColor = 'white'
+    document.getElementById('botao-mobile').style.background = 'var(--cor-lata-zero)'
+    document.getElementById('botao-mobile').style.transition = '1s all'
+}
+
